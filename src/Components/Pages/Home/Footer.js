84ed46890b87a884b45120f86151e67d4e118c70
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-05-10 22:46:42 +0600

    footer link route added

--- src/Components/Pages/Home/Footer.js
+++ src/Components/Pages/Home/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { p } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
@@ -11,27 +11,27 @@ const Footer = () => {
             <div className='footer'>
                 <div>
                     <span className="footer-title">Services</span>
-                    <p className="p p-hover">Branding</p>
-                    <p className="p p-hover">Design</p>
-                    <p className="p p-hover">Marketing</p>
-                    <p className="p p-hover">Advertisement</p>
+                    <Link path="/" className="link link-hover">Branding</Link>
+                    <Link path="/" className="link link-hover">Design</Link>
+                    <Link path="/" className="link link-hover">Marketing</Link>
+                    <Link path="/" className="link link-hover">Advertisement</Link>
                 </div>
                 <div>
                     <span className="footer-title">Company</span>
-                    <p className="p p-hover">About us</p>
-                    <p className="p p-hover">Contact</p>
-                    <p className="p p-hover">Jobs</p>
-                    <p className="p p-hover">Press kit</p>
+                    <Link path="/"  className="link link-hover">About us</Link>
+                    <Link path="/"  className="link link-hover">Contact</Link>
+                    <Link path="/"  className="link link-hover">Jobs</Link>
+                    <Link path="/"  className="link link-hover">Press kit</Link>
                 </div>
                 <div>
                     <span className="footer-title">Legal</span>
-                    <p className="p p-hover">Terms of use</p>
-                    <p className="p p-hover">Privacy policy</p>
-                    <p className="p p-hover">Cookie policy</p>
+                    <Link path="/"  className="link link-hover">Terms of use</Link>
+                    <Link path="/"  className="link link-hover">Privacy policy</Link>
+                    <Link path="/"  className="link link-hover">Cookie policy</Link>
                 </div>
             </div>
             <div className='my-10 text-center'>
-                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                <p>Copyright © 2022 - All right reserved </p>
             </div>
         </footer>
     );
