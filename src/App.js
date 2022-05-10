535f1e197b9fd047c60314e1680cf31e0d674022
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-05-10 10:57:50 +0600

    created navbar, route, and home, review, appointment,  about, login,contact us component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Shared/Navbar';
+import Home from './Components/Pages/Home/Home'
+import About from './Components/Pages/About/About'
+import Review from './Components/Pages/Reviews/Reviews'
+import Appointent from './Components/Pages/Appointment/Appointment'
+import Contact from './Components/Pages/Contact/Contact'
+import Login from './Components/Pages/Login/Login'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="appointment" element={<Appointent />} />
+        <Route path="review" element={<Review />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<Login />} />
+       
+      </Routes>
     </div>
   );
 }
